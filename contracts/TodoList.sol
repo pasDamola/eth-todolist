db0409f8bf6ad9352fb468f7b044ac696e2a4d60
oyincode <oyindadavid18@gmail.com>
2019-08-08 13:23:23 +0100

    createTask function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+     struct Task {
+        uint id;
+        string content;
+        bool completed;
+  }
+
+  constructor() public {
+        createTask("Check out dappuniversity.com");
+   }
+
+  mapping(uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
